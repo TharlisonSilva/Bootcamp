d534113a62588190c26e5ce65877e5e97aaa6cb8
unknown <tharlison.pro@gmail.com>
2023-03-31 00:18:42 -0300

    Validação do botão publicar

--- Projeto-01/src/components/post/Post.jsx
+++ Projeto-01/src/components/post/Post.jsx
@@ -28,7 +28,12 @@ export function Post({ author, publishedAt, content }){
     }
 
     function handleNewCommentChange(){
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid(){
+        event.target.setCustomValidity('Este campo é obrigátorio!');
     }
 
     function deleteComment(commentToDelete){
@@ -38,6 +43,8 @@ export function Post({ author, publishedAt, content }){
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length == 0;
 
     return (
         <article className={styles.post}>
@@ -73,10 +80,14 @@ export function Post({ author, publishedAt, content }){
                     placeholder="Deixe seu comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
 
             </form>
