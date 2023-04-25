1f07d6d2868c6886e339ba02d7ab6d4c07c5d156
unknown <tharlison.pro@gmail.com>
2023-04-24 22:31:46 -0300

    commit inicial - projeto pratico/revisão do conteudo

---
+++ 01-projeto-pactico-reactjs/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import styles from './Header.module.css'
+import imgIcon from '../assets/rocket.svg';
+import { PlusCircle } from 'phosphor-react';
+
+export function Header(){
+    return (
+
+        <header className={styles.header}>
+            
+            <div className={styles.container}>
+                <img src={imgIcon} alt='Logo do TodoList' />
+                <div className={styles.nameLogo}>
+                    <h1 className={styles.to}>to</h1>
+                    <h1 className={styles.do}>do</h1>
+                </div>
+            </div>
+
+            <div className={styles.InputTask}>
+                <input type="text" />
+                <button>Criar <PlusCircle size={12}/> </button>
+            </div>
+            
+        </header>
+
+    )
+}
